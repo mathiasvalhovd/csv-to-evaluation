1ad8e2f3cd56ae0a702affc459e1a3df8d61b1d1
Mathias Valhovd <76526229+mathiasvalhovd@users.noreply.github.com>
2023-09-27 23:27:17 +0200

    set up connection between backend and frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,40 @@
 import React, { ChangeEvent, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Papa from 'papaparse'
+import Papa from 'papaparse';
+import axios from 'axios';
 
 function App() {
-
   const [data, setData] = useState<any[]>([]);
 
   const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
       Papa.parse(file, {
-        header: true, 
+        header: true,
         complete: (results) => {
           setData(results.data);
         },
       });
-    };
+    }
   };
 
+  const sendDataToBackend = () => {
+    axios
+      .post('http://localhost:5000/upload', { data })
+      .then((response) => {
+        console.log('Data sent to Flask backend:', response.data);
+        // You can perform actions based on the response from the backend here
+      })
+      .catch((error) => {
+        return "HEI";
+      });
+  };
 
   return (
     <div className="App-header">
       <p>La meg lage din evaluering for deg! Bare last opp filen din under her</p>
-      <input type="file" accept='.csv' onChange={handleFileUpload}/>
+      <input name="data" type="file" accept=".csv" onChange={handleFileUpload} />
+      <button onClick={sendDataToBackend}>Send Data to Backend</button>
     </div>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/added-file")
-def added_file():
-    return "hei"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/upload', methods=['POST'])
+def upload_data():
+    try:
+        data = request.json.get('data', [])
+        print("recieved data:", data)
+        return jsonify({'message': 'Data received successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
     
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
